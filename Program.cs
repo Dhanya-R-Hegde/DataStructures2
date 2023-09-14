f4ff5ffa83a758409cda90248701d8eede7861ec
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-15 00:17:40 +0530

    [Dhanya R Hegde] : Added BST UserCase2

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace DataStructures2
         {
 
             //UserCase1
-            BinarySearchTree binarySearchTree = new BinarySearchTree();
-            int[]values = { 56, 30, 70};
+            //BinarySearchTree binarySearchTree = new BinarySearchTree();
+            //int[]values = { 56, 30, 70};
+            //Node root = null;
+
+            //for (int i = 0; i < values.Length; i++)
+            //{
+            //    root = insert(root, values[i]);
+            //}
+
+            //inorder(root);
+            //Console.WriteLine();
+            //Console.WriteLine(Node.size);
+
+            //UserCase2
+            int []values = { 56, 30, 40, 22, 11, 16, 3, 70, 60, 95, 65, 63, 67 };
             Node root = null;
 
             for (int i = 0; i < values.Length; i++)
